31bbbddb274ea45c1470812896bc67304900b00e
baddamsreejareddy <baddamsreejareddy@gmail.com>
2026-06-06 11:31:18 +0530

    Add program to subtract two pointer addresses

---
+++ subtraction of two pointers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,*ptr1,*ptr2;
+ptr1=&a;
+ptr2=&b;
+printf("enter value for a:");
+scanf("%d",&a);
+printf("enter value for b:");
+scanf("%d",&b);
+printf("ptr1=%d,ptr2=%d",ptr1,ptr2);
+ptr1-ptr2;
+printf("%d",ptr1-ptr2);
+return 0;
+}
+
